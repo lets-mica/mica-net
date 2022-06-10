e749699c6a44bcaa709477cc8eb9dc9c3d58b573
如梦技术 <596392912@qq.com>
2022-06-10 11:59:15 +0800

    :sparkles: 代码优化

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 1. 使用 java8 作为最低编译版本。
 2. 去除了一些使用不到的模块和代码。
 3. 切换到 cache2k、使用 fastjson2，mica-net-core 不依赖 fastjson。
-4. 代码优化。
+4. 代码优化，更加符合规范。
--- mica-net-core/src/main/java/org/tio/client/ReconnConf.java
+++ mica-net-core/src/main/java/org/tio/client/ReconnConf.java
@@ -2,46 +2,46 @@
 	Apache License
 	Version 2.0, January 2004
 	http://www.apache.org/licenses/
-	
+
 	TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
-	
+
 	1. Definitions.
-	
+
 	"License" shall mean the terms and conditions for use, reproduction, and
 	distribution as defined by Sections 1 through 9 of this document.
-	
+
 	"Licensor" shall mean the copyright owner or entity authorized by the copyright
 	owner that is granting the License.
-	
+
 	"Legal Entity" shall mean the union of the acting entity and all other entities
 	that control, are controlled by, or are under common control with that entity.
 	For the purposes of this definition, "control" means (i) the power, direct or
 	indirect, to cause the direction or management of such entity, whether by
 	contract or otherwise, or (ii) ownership of fifty percent (50%) or more of the
 	outstanding shares, or (iii) beneficial ownership of such entity.
-	
+
 	"You" (or "Your") shall mean an individual or Legal Entity exercising
 	permissions granted by this License.
-	
+
 	"Source" form shall mean the preferred form for making modifications, including
 	but not limited to software source code, documentation source, and configuration
 	files.
-	
+
 	"Object" form shall mean any form resulting from mechanical transformation or
 	translation of a Source form, including but not limited to compiled object code,
 	generated documentation, and conversions to other media types.
-	
+
 	"Work" shall mean the work of authorship, whether in Source or Object form, made
 	available under the License, as indicated by a copyright notice that is included
 	in or attached to the work (an example is provided in the Appendix below).
-	
+
 	"Derivative Works" shall mean any work, whether in Source or Object form, that
 	is based on (or derived from) the Work and for which the editorial revisions,
 	annotations, elaborations, or other modifications represent, as a whole, an
 	original work of authorship. For the purposes of this License, Derivative Works
 	shall not include works that remain separable from, or merely link (or bind by
 	name) to the interfaces of, the Work and Derivative Works thereof.
-	
+
 	"Contribution" shall mean any work of authorship, including the original version
 	of the Work and any modifications or additions to that Work or Derivative Works
 	thereof, that is intentionally submitted to Licensor for inclusion in the Work
@@ -54,21 +54,21 @@
 	the purpose of discussing and improving the Work, but excluding communication
 	that is conspicuously marked or otherwise designated in writing by the copyright
 	owner as "Not a Contribution."
-	
+
 	"Contributor" shall mean Licensor and any individual or Legal Entity on behalf
 	of whom a Contribution has been received by Licensor and subsequently
 	incorporated within the Work.
-	
+
 	2. Grant of Copyright License.
-	
+
 	Subject to the terms and conditions of this License, each Contributor hereby
 	grants to You a perpetual, worldwide, non-exclusive, no-charge, royalty-free,
 	irrevocable copyright license to reproduce, prepare Derivative Works of,
 	publicly display, publicly perform, sublicense, and distribute the Work and such
 	Derivative Works in Source or Object form.
-	
+
 	3. Grant of Patent License.
-	
+
 	Subject to the terms and conditions of this License, each Contributor hereby
 	grants to You a perpetual, worldwide, non-exclusive, no-charge, royalty-free,
 	irrevocable (except as stated in this section) patent license to make, have
@@ -81,13 +81,13 @@
 	Contribution incorporated within the Work constitutes direct or contributory
 	patent infringement, then any patent licenses granted to You under this License
 	for that Work shall terminate as of the date such litigation is filed.
-	
+
 	4. Redistribution.
-	
+
 	You may reproduce and distribute copies of the Work or Derivative Works thereof
 	in any medium, with or without modifications, and in Source or Object form,
 	provided that You meet the following conditions:
-	
+
 	You must give any other recipients of the Work or Derivative Works a copy of
 	this License; and
 	You must cause any modified files to carry prominent notices stating that You
@@ -114,25 +114,25 @@
 	distribution of Your modifications, or for any such Derivative Works as a whole,
 	provided Your use, reproduction, and distribution of the Work otherwise complies
 	with the conditions stated in this License.
-	
+
 	5. Submission of Contributions.
-	
+
 	Unless You explicitly state otherwise, any Contribution intentionally submitted
 	for inclusion in the Work by You to the Licensor shall be under the terms and
 	conditions of this License, without any additional terms or conditions.
 	Notwithstanding the above, nothing herein shall supersede or modify the terms of
 	any separate license agreement you may have executed with Licensor regarding
 	such Contributions.
-	
+
 	6. Trademarks.
-	
+
 	This License does not grant permission to use the trade names, trademarks,
 	service marks, or product names of the Licensor, except as required for
 	reasonable and customary use in describing the origin of the Work and
 	reproducing the content of the NOTICE file.
-	
+
 	7. Disclaimer of Warranty.
-	
+
 	Unless required by applicable law or agreed to in writing, Licensor provides the
 	Work (and each Contributor provides its Contributions) on an "AS IS" BASIS,
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
@@ -141,9 +141,9 @@
 	solely responsible for determining the appropriateness of using or
 	redistributing the Work and assume any risks associated with Your exercise of
 	permissions under this License.
-	
+
 	8. Limitation of Liability.
-	
+
 	In no event and under no legal theory, whether in tort (including negligence),
 	contract, or otherwise, unless required by applicable law (such as deliberate
 	and grossly negligent acts) or agreed to in writing, shall any Contributor be
@@ -153,9 +153,9 @@
 	damages for loss of goodwill, work stoppage, computer failure or malfunction, or
 	any and all other commercial damages or losses), even if such Contributor has
 	been advised of the possibility of such damages.
-	
+
 	9. Accepting Warranty or Additional Liability.
-	
+
 	While redistributing the Work or Derivative Works thereof, You may choose to
 	offer, and charge a fee for, acceptance of support, warranty, indemnity, or
 	other liability obligations and/or rights consistent with this License. However,
@@ -164,11 +164,11 @@
 	agree to indemnify, defend, and hold each Contributor harmless for any liability
 	incurred by, or claims asserted against, such Contributor by reason of your
 	accepting any such warranty or additional liability.
-	
+
 	END OF TERMS AND CONDITIONS
-	
+
 	APPENDIX: How to apply the Apache License to your work
-	
+
 	To apply the Apache License to your work, attach the following boilerplate
 	notice, with the fields enclosed by brackets "{}" replaced with your own
 	identifying information. (Don't include the brackets!) The text should be
@@ -176,15 +176,15 @@
 	recommend that a file or class name and description of purpose be included on
 	the same "printed page" as the copyright notice for easier identification within
 	third-party archives.
-	
+
 	   Copyright 2020 t-io
-	
+
 	   Licensed under the Apache License, Version 2.0 (the "License");
 	   you may not use this file except in compliance with the License.
 	   You may obtain a copy of the License at
-	
+
 	     http://www.apache.org/licenses/LICENSE-2.0
-	
+
 	   Unless required by applicable law or agreed to in writing, software
 	   distributed under the License is distributed on an "AS IS" BASIS,
 	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -193,8 +193,6 @@
 */
 package org.tio.client;
 
-import java.util.concurrent.LinkedBlockingQueue;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tio.core.ChannelContext;
@@ -202,18 +200,18 @@ import org.tio.utils.SystemTimer;
 import org.tio.utils.thread.pool.DefaultThreadFactory;
 import org.tio.utils.thread.pool.SynThreadPoolExecutor;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
- *
  * @author tanyaowu
  * 2017年4月1日 上午9:33:00
  */
 public class ReconnConf {
-	private static Logger log = LoggerFactory.getLogger(ReconnConf.class);
+	private static final Logger log = LoggerFactory.getLogger(ReconnConf.class);
 
 	public static ReconnConf getReconnConf(ClientChannelContext clientChannelContext) {
 		TioClientConfig tioClientConfig = (TioClientConfig) clientChannelContext.tioConfig;
-		ReconnConf reconnConf = tioClientConfig.getReconnConf();
-		return reconnConf;
+		return tioClientConfig.getReconnConf();
 	}
 
 	public static ReconnConf getReconnConf(ChannelContext channelContext) {
@@ -221,9 +219,8 @@ public class ReconnConf {
 	}
 
 	/**
-	 * 
 	 * @param clientChannelContext
-	 * @param putIfNeedConn 如果需要重连，则把该ClientChannelContext放到重连队列中
+	 * @param putIfNeedConn        如果需要重连，则把该ClientChannelContext放到重连队列中
 	 * @return
 	 */
 	public static boolean isNeedReconn(ClientChannelContext clientChannelContext, boolean putIfNeedConn) {
@@ -234,7 +231,6 @@ public class ReconnConf {
 		if (reconnConf == null) {
 			return false;
 		}
-
 		if (reconnConf.getInterval() > 0) {
 			if (reconnConf.getRetryCount() <= 0 || reconnConf.getRetryCount() > clientChannelContext.getReconnCount().get()) {
 				if (putIfNeedConn) {
@@ -243,14 +239,12 @@ public class ReconnConf {
 					clientChannelContext.stat.timeInReconnQueue = SystemTimer.currTime;
 					reconnConf.getQueue().add(clientChannelContext);
 				}
-
 				return true;
 			} else {
 				log.info("不需要重连{}", clientChannelContext);
 				return false;
 			}
 		}
-
 		return false;
 	}
 
@@ -262,8 +256,6 @@ public class ReconnConf {
 		if (clientChannelContext == null) {
 			return false;
 		}
-
-		//		ReconnConf reconnConf = ReconnConf.getReconnConf(clientChannelContext);	
 		return isNeedReconn(clientChannelContext, true);
 	}
 
@@ -282,44 +274,22 @@ public class ReconnConf {
 	/**
 	 * 用来重连的线程池
 	 */
-	private volatile SynThreadPoolExecutor threadPoolExecutor = null;
+	private final SynThreadPoolExecutor threadPoolExecutor;
 
 	/**
-	 *
-	 *
 	 * @author tanyaowu
-	 *
 	 */
 	public ReconnConf() {
-		if (threadPoolExecutor == null) {
-			synchronized (ReconnConf.class) {
-				if (threadPoolExecutor == null) {
-					//					threadPoolExecutor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), Runtime.getRuntime().availableProcessors(), 60L, TimeUnit.SECONDS,
-					//							new LinkedBlockingQueue<Runnable>(), DefaultThreadFactory.getInstance("tio-client-reconn"));
-					//					
-					//					
-
-					LinkedBlockingQueue<Runnable> tioQueue = new LinkedBlockingQueue<>();
-					//			ArrayBlockingQueue<Runnable> tioQueue = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
-					String tioThreadName = "tio-client-reconn";
-					DefaultThreadFactory defaultThreadFactory = DefaultThreadFactory.getInstance(tioThreadName, Thread.MAX_PRIORITY);
-
-					threadPoolExecutor = new SynThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), Runtime.getRuntime().availableProcessors(), 60L, tioQueue,
-					        defaultThreadFactory, tioThreadName);
-					//			tioExecutor = new SynThreadPoolExecutor(AVAILABLE_PROCESSORS * 2, Integer.MAX_VALUE, 60, new SynchronousQueue<Runnable>(), defaultThreadFactory, tioThreadName);
-
-				}
-			}
-
-		}
-
+		LinkedBlockingQueue<Runnable> tioQueue = new LinkedBlockingQueue<>();
+		String tioThreadName = "tio-client-reconn";
+		DefaultThreadFactory defaultThreadFactory = DefaultThreadFactory.getInstance(tioThreadName, Thread.MAX_PRIORITY);
+		// 重连一般都是客户端重连，只用一个线程即可
+		threadPoolExecutor = new SynThreadPoolExecutor(1, 1, 60L, tioQueue, defaultThreadFactory, tioThreadName);
 	}
 
 	/**
 	 * @param interval
-	 *
 	 * @author tanyaowu
-	 *
 	 */
 	public ReconnConf(long interval) {
 		this();
@@ -329,9 +299,7 @@ public class ReconnConf {
 	/**
 	 * @param interval
 	 * @param retryCount
-	 *
 	 * @author tanyaowu
-	 *
 	 */
 	public ReconnConf(long interval, int retryCount) {
 		this();
@@ -355,6 +323,7 @@ public class ReconnConf {
 
 	/**
 	 * 连续重连次数，当连续重连这么多次都失败时，不再重连。0和负数则一直重连
+	 *
 	 * @return the retryCount
 	 */
 	public int getRetryCount() {
@@ -377,18 +346,11 @@ public class ReconnConf {
 
 	/**
 	 * 连续重连次数，当连续重连这么多次都失败时，不再重连。0和负数则一直重连
+	 *
 	 * @param retryCount the retryCount to set
 	 */
 	public void setRetryCount(int retryCount) {
 		this.retryCount = retryCount;
 	}
-
-	//	/**
-	//	 * @param threadPoolExecutor the threadPoolExecutor to set
-	//	 */
-	//	public void setThreadPoolExecutor(ThreadPoolExecutor threadPoolExecutor)
-	//	{
-	//		this.threadPoolExecutor = threadPoolExecutor;
-	//	}
 
 }
