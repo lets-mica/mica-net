46a5c5b9eaacad381e2d557bb797904ac7325ff5
如梦技术 <596392912@qq.com>
2022-08-15 10:10:44 +0800

    :sparkles: 代码完善。

--- mica-net-core/src/main/java/org/tio/client/TioClientConfig.java
+++ mica-net-core/src/main/java/org/tio/client/TioClientConfig.java
@@ -359,6 +359,16 @@ public class TioClientConfig extends TioConfig {
 	}
 
 	@Override
+	public boolean equals(Object o) {
+		return super.equals(o);
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
 	public String toString() {
 		return "TioClientConfig [name=" + name + "]";
 	}
--- mica-net-core/src/main/java/org/tio/server/TioServerConfig.java
+++ mica-net-core/src/main/java/org/tio/server/TioServerConfig.java
@@ -451,6 +451,16 @@ public class TioServerConfig extends TioConfig {
 	}
 
 	@Override
+	public boolean equals(Object o) {
+		return super.equals(o);
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
 	public String toString() {
 		return "TioServerConfig [name=" + name + "]";
 	}
