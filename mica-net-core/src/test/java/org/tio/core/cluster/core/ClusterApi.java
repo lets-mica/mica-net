551000e49c683719acb602a3d9bee26ee738cfc2
如梦技术 <596392912@qq.com>
2023-05-06 15:38:58 +0800

    :sparkles: 构思 tcp 集群

--- mica-net-core/src/test/java/org/tio/core/cluster/core/ClusterApi.java
+++ mica-net-core/src/test/java/org/tio/core/cluster/core/ClusterApi.java
@@ -1,6 +1,9 @@
 package org.tio.core.cluster.core;
 
+import org.tio.core.Node;
 import org.tio.core.cluster.message.ClusterDataMessage;
+
+import java.util.Collection;
 
 /**
  * 集群接口
@@ -20,6 +23,24 @@ public interface ClusterApi {
 	void stop();
 
 	/**
+	 * 发送消息
+	 *
+	 * @param address address
+	 * @param message 集群消息
+	 * @return 消息id.
+	 */
+	String send(Node address, ClusterDataMessage message);
+
+	/**
+	 * 发送消息
+	 *
+	 * @param member  ClusterMember
+	 * @param message 集群消息
+	 * @return 消息id.
+	 */
+	String send(ClusterMember member, ClusterDataMessage message);
+
+	/**
 	 * 在集群中广播消息
 	 *
 	 * @param message 集群消息
@@ -31,5 +52,40 @@ public interface ClusterApi {
 	 * 集群消息监听
 	 */
 	void listen(ClusterMessageListener listener);
+
+	/**
+	 * 获取集群中的成员，包含当前成员
+	 *
+	 * @return 成员列表
+	 */
+	Collection<ClusterMember> getMembers();
+
+	/**
+	 * 获取集群中的成员，不包含当前成员
+	 *
+	 * @return 成员列表，不包含自己
+	 */
+	Collection<ClusterMember> getOtherMembers();
+
+	/**
+	 * 获取本地成员
+	 *
+	 * @return 本地成员
+	 */
+	ClusterMember getMember();
+
+	/**
+	 * 根据成员 id 获取成员
+	 *
+	 * @return 成员
+	 */
+	ClusterMember getMember(String id);
+
+	/**
+	 * 更加地址获取成员
+	 *
+	 * @return 成员
+	 */
+	ClusterMember member(Node address);
 
 }
--- mica-net-core/src/test/java/org/tio/core/cluster/core/ClusterImpl.java
+++ mica-net-core/src/test/java/org/tio/core/cluster/core/ClusterImpl.java
@@ -1,6 +1,9 @@
 package org.tio.core.cluster.core;
 
+import org.tio.core.Node;
 import org.tio.core.cluster.message.ClusterDataMessage;
+
+import java.util.Collection;
 
 /**
  * 集群实现
@@ -19,6 +22,16 @@ public class ClusterImpl implements ClusterApi {
 	}
 
 	@Override
+	public String send(Node address, ClusterDataMessage message) {
+		return null;
+	}
+
+	@Override
+	public String send(ClusterMember member, ClusterDataMessage message) {
+		return null;
+	}
+
+	@Override
 	public String broadcast(ClusterDataMessage message) {
 		return null;
 	}
@@ -26,5 +39,30 @@ public class ClusterImpl implements ClusterApi {
 	@Override
 	public void listen(ClusterMessageListener listener) {
 
+	}
+
+	@Override
+	public Collection<ClusterMember> getMembers() {
+		return null;
+	}
+
+	@Override
+	public Collection<ClusterMember> getOtherMembers() {
+		return null;
+	}
+
+	@Override
+	public ClusterMember getMember() {
+		return null;
+	}
+
+	@Override
+	public ClusterMember getMember(String id) {
+		return null;
+	}
+
+	@Override
+	public ClusterMember member(Node address) {
+		return null;
 	}
 }
