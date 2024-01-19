a07f32a03bdc03aa81bc4add799b29617f6d2eb5
如梦技术 <596392912@qq.com>
2024-01-19 17:35:50 +0800

    :sparkles: 考虑 udp 可以使用 tcp 的编码器

---
+++ mica-net-core/src/test/java/org/tio/core/udp/core/intf/UdpHandler.java
@@ -0,0 +1,11 @@
+package org.tio.core.udp.core.intf;
+
+import org.tio.core.intf.TioHandler;
+
+/**
+ * udp 处理器
+ *
+ * @author L.cm
+ */
+public interface UdpHandler extends TioHandler {
+}
---
+++ mica-net-core/src/test/java/org/tio/core/udp/core/intf/UdpListener.java
@@ -0,0 +1,11 @@
+package org.tio.core.udp.core.intf;
+
+import org.tio.core.intf.TioListener;
+
+/**
+ * udp 监听器，udp 没有连接和断开连接
+ *
+ * @author L.cm
+ */
+public interface UdpListener extends TioListener {
+}
