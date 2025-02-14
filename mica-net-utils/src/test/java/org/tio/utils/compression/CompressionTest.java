5f56ee60c599e2366b3e7c7d7e7d2d124c27f3d8
如梦技术 <596392912@qq.com>
2025-02-14 14:50:05 +0800

    :sparkles: 压缩接口下沉

---
+++ mica-net-utils/src/test/java/org/tio/utils/compression/CompressionTest.java
@@ -0,0 +1,26 @@
+package org.tio.utils.compression;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Compression 测试
+ *
+ * @author L.cm
+ */
+class CompressionTest {
+
+	@Test
+	void test() throws IOException {
+		String text = "如梦技术——你的编程加油站！";
+		GzipCompression compression = new GzipCompression();
+		byte[] bytes = compression.compress(text.getBytes(StandardCharsets.UTF_8));
+		byte[] decompress = compression.decompress(bytes);
+		String s = new String(decompress, StandardCharsets.UTF_8);
+		Assertions.assertEquals(text, s);
+	}
+
+}
