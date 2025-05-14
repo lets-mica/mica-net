b0aecbd99d8ee839864fcaabd901cc4a78bddf5a
如梦技术 <596392912@qq.com>
2025-05-14 16:40:22 +0800

    :sparkles: 代码简化

--- mica-net-core/src/main/java/org/tio/server/cluster/core/ClusterTcpServerListener.java
+++ mica-net-core/src/main/java/org/tio/server/cluster/core/ClusterTcpServerListener.java
@@ -30,15 +30,6 @@ public class ClusterTcpServerListener implements TioServerListener {
 	private static final Logger log = LoggerFactory.getLogger(ClusterTcpServerListener.class);
 
 	@Override
-	public boolean onHeartbeatTimeout(ChannelContext context, long interval, int heartbeatTimeoutCount) {
-		return false;
-	}
-
-	@Override
-	public void onAfterConnected(ChannelContext context, boolean isConnected, boolean isReconnect) throws Exception {
-	}
-
-	@Override
 	public void onBeforeClose(ChannelContext context, Throwable throwable, String remark, boolean isRemove) throws Exception {
 		if (throwable != null) {
 			log.error(throwable.getMessage(), throwable);
