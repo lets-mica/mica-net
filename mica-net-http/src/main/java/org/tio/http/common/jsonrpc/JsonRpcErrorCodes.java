2fe1ccdb3689f0e9065c37ce0f54f089473a0676
如梦技术 <596392912@qq.com>
2025-05-29 18:04:36 +0800

    :sparkles: 开始开发 jsonrpc 为 mcp 做准备

---
+++ mica-net-http/src/main/java/org/tio/http/common/jsonrpc/JsonRpcErrorCodes.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & dreamlu.net).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tio.http.common.jsonrpc;
+
+/**
+ * JSON-RPC Error Codes
+ *
+ * @author L.cm
+ */
+public interface JsonRpcErrorCodes {
+
+	/**
+	 * Invalid JSON was received by the server.
+	 */
+	int PARSE_ERROR = -32700;
+
+	/**
+	 * The JSON sent is not a valid Request object.
+	 */
+	int INVALID_REQUEST = -32600;
+
+	/**
+	 * The method does not exist / is not available.
+	 */
+	int METHOD_NOT_FOUND = -32601;
+
+	/**
+	 * Invalid method parameter(s).
+	 */
+	int INVALID_PARAMS = -32602;
+
+	/**
+	 * Internal JSON-RPC error.
+	 */
+	int INTERNAL_ERROR = -32603;
+
+}
--- mica-net-http/src/main/moditect/module-info.java
+++ mica-net-http/src/main/moditect/module-info.java
@@ -4,6 +4,7 @@ open module net.dreamlu.mica.net.http {
 	exports org.tio.http.common;
 	exports org.tio.http.common.handler;
 	exports org.tio.http.common.jsonrpc;
+	exports org.tio.http.common.mcp;
 	exports org.tio.http.common.sse;
 	exports org.tio.http.common.utils;
 	exports org.tio.http.server;
