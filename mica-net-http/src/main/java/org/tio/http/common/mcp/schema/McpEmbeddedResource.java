d064d3839294e20a00c574be8bb3a013f22408d5
如梦技术 <596392912@qq.com>
2025-06-03 22:32:55 +0800

    :sparkles: 调试 mcp

---
+++ mica-net-http/src/main/java/org/tio/http/common/mcp/schema/McpEmbeddedResource.java
@@ -0,0 +1,7 @@
+package org.tio.http.common.mcp.schema;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+public class McpEmbeddedResource implements McpContent {
+}
