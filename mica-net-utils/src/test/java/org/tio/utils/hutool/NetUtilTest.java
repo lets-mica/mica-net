7f3551e570917f7aed14053617125a76b99fffff
如梦技术 <596392912@qq.com>
2025-07-24 14:02:20 +0800

    :sparkles: 开始1.1.16-SNAPSHOT

--- mica-net-utils/src/test/java/org/tio/utils/hutool/NetUtilTest.java
+++ mica-net-utils/src/test/java/org/tio/utils/hutool/NetUtilTest.java
@@ -15,15 +15,14 @@ public class NetUtilTest {
 		Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 		while (interfaces.hasMoreElements()) {
 			NetworkInterface networkInterface = interfaces.nextElement();
-			// 跳过回环接口和未启用的接口
-			if (networkInterface.isLoopback() || !networkInterface.isUp()) {
-				continue;
-			}
 			// 获取接口名称
 			String interfaceName = networkInterface.getName();
-			System.out.println("网卡名称: " + interfaceName);
-			System.out.println("显示名称: " + networkInterface.getDisplayName());
-			System.out.println("IP地址: " + NetUtil.getNetworkInterfaceIpV4(interfaceName));
+			String ipV4 = NetUtil.getNetworkInterfaceIpV4(interfaceName);
+			if (StrUtil.isNotBlank(ipV4)) {
+				System.out.println("网卡名称: " + interfaceName);
+				System.out.println("显示名称: " + networkInterface.getDisplayName());
+				System.out.println("IP地址: " + ipV4);
+			}
 		}
 	}
 
